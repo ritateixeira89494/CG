cd848515225cb34e55b6ef9f70df004fcc76cd4e
Diogo Manuel Brito Pires <30441515+El-Banderas@users.noreply.github.com>
2022-03-08 20:02:27 +0000

    box.cpp

---
+++ srcDR/Box.h
@@ -0,0 +1,12 @@
+#ifndef BOX_H
+#define BOX_H
+
+void drawSquareUp(float xOr, float yOr, float side, float hight);
+void drawSquareDown(float xOr, float yOr, float side);
+void drawSquareXY(float xOr, float yOr, float side);
+void drawSquareXY(float xOr, float yOr, float side, float z);
+void drawSquareYZ(float yOr, float zOr, float side);
+void drawSquareYZ(float yOr, float zOr, float, float x);
+
+
+#endif // BOX_H
