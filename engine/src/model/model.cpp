37ec45a5c16b4fe51eba2bff7504932a7bad55a5
Mapc1 <marcocosta579@gmail.com>
2022-03-12 01:47:41 +0000

    Remove line number from file parsing

--- engine/src/model/model.cpp
+++ engine/src/model/model.cpp
@@ -20,15 +20,14 @@ namespace model {
         GLfloat point2[3];
         GLfloat point3[3];
 
-        fscanf(f, "%d\n", &n_triangles); 
-
         int mem = n_triangles * 9;
-        for(int i = 0; i < mem; i+=9) {
+        while(
             fscanf(f, "(%f,%f,%f);(%f,%f,%f);(%f,%f,%f)\n",
-                   &point1[0], &point1[1], &point1[2],
-                   &point2[0], &point2[1], &point2[2],
-                   &point3[0], &point3[1], &point3[2]
-                   );
+                &point1[0], &point1[1], &point1[2],
+                &point2[0], &point2[1], &point2[2],
+                &point3[0], &point3[1], &point3[2]
+            ) != EOF
+        ) {
             triangles.push_back(new Triangle(
                 point1[0], point1[1], point1[2],
                 point2[0], point2[1], point2[2],
