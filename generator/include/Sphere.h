43c2eb871d32f72c174fdbd6cbefa1452dc75b2b
Diogo Manuel Brito Pires <30441515+El-Banderas@users.noreply.github.com>
2022-03-13 11:21:00 +0000

    Doc

--- generator/include/Sphere.h
+++ generator/include/Sphere.h
@@ -2,7 +2,9 @@
 #define __SPHERE_H__
 
 #include <string.h>
-
+/*!
+    In this function we open and close the necessary file, and draw to the file the points that belong to the sphere.
+*/
 void drawSphere(int radius, int slices, int stacks, std::string filename);
 
 #endif // __SPHERE_H__
