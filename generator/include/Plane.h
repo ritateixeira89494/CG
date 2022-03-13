87b6047ef53c83955f31b71a8418125238e69935
Diogo Manuel Brito Pires <30441515+El-Banderas@users.noreply.github.com>
2022-03-13 11:18:43 +0000

    Doc

--- generator/include/Plane.h
+++ generator/include/Plane.h
@@ -5,9 +5,17 @@
 
 using namespace std;
 
+/*!
+  This two functions are used in the design of the box, but they draw the same planes as the necessary in plane design.
+  So, they need to be public, and in this file.
+*/
 void drawSquareUp(float xOr, float yOr, float side, float height, ofstream *file);
 void drawSquareDown(float xOr, float yOr, float edge, ofstream *file);
 
+/*!
+    Draw a plane from individual points, line by line.
+    His behaviour is described in the report.
+*/
 void drawPlane(float length, float divisions, string name);
 
 #endif // __PLANE_H__
