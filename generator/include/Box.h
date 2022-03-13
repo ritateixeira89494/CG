23e94d17d3d30484ed1e1ca93a2fe6b0c8e2d2c4
Diogo Manuel Brito Pires <30441515+El-Banderas@users.noreply.github.com>
2022-03-13 11:10:18 +0000

    Documentation

--- generator/include/Box.h
+++ generator/include/Box.h
@@ -2,6 +2,9 @@
 #define __BOX_H__
 
 #include <string.h>
-
+/*!
+    Draw a box from individual points, line by line.
+    Here, we only use two cycles.
+*/
 void drawBox(float length, float divisions, std::string nameOfFile);
 #endif // __BOX_H__
