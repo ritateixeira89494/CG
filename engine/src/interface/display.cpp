9020b6aa62abc2af9da0a39b7d1a3d966d3301f1
Gonçalo André Rodrigues Soares <62038187+gonaloppcc@users.noreply.github.com>
2022-03-19 19:32:06 +0000

    Fix everything 2.

--- engine/src/interface/display.cpp
+++ engine/src/interface/display.cpp
@@ -24,16 +24,16 @@ void placeAxis() {
     glBegin(GL_LINES);
     // X Axis in red
     glColor3f(1.0f, 0.0f, 0.0f);
-    glVertex3f(0.0f, 0.0f, 0.0f);
-    glVertex3f(10.0f, 0.0f, 0.0f);
+    glVertex3f(-100.0f, 0.0f, 0.0f);
+    glVertex3f(100.0f, 0.0f, 0.0f);
     // Y Axis in Green
     glColor3f(0.0f, 1.0f, 0.0f);
-    glVertex3f(0.0f, 0.0f, 0.0f);
-    glVertex3f(0.0f, 10.0f, 0.0f);
+    glVertex3f(0.0f, -100.0f, 0.0f);
+    glVertex3f(0.0f, 100.0f, 0.0f);
     // Z Axis in Blue
     glColor3f(0.0f, 0.0f, 1.0f);
-    glVertex3f(0.0f, 0.0f, 0.0f);
-    glVertex3f(0.0f, 0.0f, 10.0f);
+    glVertex3f(0.0f, 0.0f, -100.0f);
+    glVertex3f(0.0f, 0.0f, 100.0f);
     glEnd();
 }
 
