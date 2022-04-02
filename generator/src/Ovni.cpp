59ab1f5b51dc26619bafe45aca83b95f717f3622
El-Banderas <diogobpires@gmail.com>
2022-04-02 20:25:48 +0100

    ovni xml

---
+++ generator/src/Ovni.cpp
@@ -0,0 +1,5 @@
+//
+// Created by banderas on 29‏/3‏/2022.
+//
+
+#include "Ovni.h"
---
+++ generator/src/Ovni.h
@@ -0,0 +1,14 @@
+//
+// Created by banderas on 29‏/3‏/2022.
+//
+
+#ifndef CG2022_OVNI_H
+#define CG2022_OVNI_H
+
+
+class Ovni {
+
+};
+
+
+#endif //CG2022_OVNI_H
