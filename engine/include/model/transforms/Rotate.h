849596059f55c9227a31135476cbe62b5f6f5afb
Mapc1 <marcocosta579@gmail.com>
2022-04-29 11:48:52 +0100

    Add comments to Rotate

--- engine/include/model/transforms/Rotate.h
+++ engine/include/model/transforms/Rotate.h
@@ -29,23 +29,48 @@ class Rotate : public Transform {
 
     public:
 // Constructors ///////////////////////////////////////////////////////////////////////////////////
+        /**
+         * @brief Construct a new static rotation
+         * 
+         * @param angle Angle of rotation
+         * @param x X coordinate
+         * @param y Y coordinate
+         * @param z Z coordinate
+         */
         Rotate(float angle, float x, float y, float z);
+        /**
+         * @brief Construct a new dynamic rotation
+         * 
+         * @param t Full length of rotation in seconds
+         * @param x X coordinate
+         * @param y Y coordinate
+         * @param z Z coordinate
+         */
         Rotate(int t, float x, float y, float z);
 
 // Methods ////////////////////////////////////////////////////////////////////////////////////////
+        /// @brief Applies the rotation, according to it's dynamism
         void apply() override;
 
 // Getters/Setters ////////////////////////////////////////////////////////////////////////////////
+        /// @brief Sets the dynamic variable
         void set_dynamic(bool dynamic);
+        /// @brief Gets the dynamic variable
         bool get_dynamic();
 
+        /// @brief Sets the angle of rotation
         void set_angle(float angle);
+        /// @brief Gets the angle of rotation
         float get_angle();
 
+        /// @brief Sets the full animation length
         void set_full_time(int full_time);
+        /// @brief Gets the full animation length
         int get_full_time();
 
+        /// @brief Sets the animation start
         void set_start(int start);
+        /// @brief Gets the animation start
         int get_start();
 };
 
